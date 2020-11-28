2db0dfa786082e403a03b41487f7bb1503c93c8e
VladimirBorisevich <borisevich.vd101@gmail.com>
2020-11-28 13:42:20 +0300

    added code implementation

--- binary tree.py
+++ binary tree.py
@@ -1,26 +1,63 @@
-
 class Node:
-    def __init__(self,value):
+    def __init__(self, value, left=None, right=None):
         self.value = value
+        self.left = left
+        self.right = right
 
-class Binary_tree:
+
+class BinaryTree:
 
     def __init__(self):
-        self.head = None
-        self.left = None
-        self.right = None
+        self.root = None
+        self.counter = 0
 
     def push(self, item):
+        self.root = self.__push(self.root, item)
+        self.counter += 1
 
-        if self.head == None:
-            self.head = Node(item)
+    def __push(self, node, item):
+        if node is None:
+            return Node(item)
+
+        if item < node.value:
+            node.left = self.__push(node.left, item)
+        else:
+            node.right = self.__push(node.right, item)
+        return node
+
+    def size(self):
+        return self.counter
+
+    def print_values(self):
+        return self.__print(self.root)
+
+    def __print(self, node):
+        if node is None:
             return
+        print(node.value, end=' ')
+        self.__print(node.left)
+        self.__print(node.right)
 
-        if item < self.head
-            self.left = Node(item)
+    def contains(self, item):
+        temp = self.root
+        while temp:
+            if temp.value == item:
+                return True
+            if item > temp.value:
+                temp = temp.right
+            else:
+                temp = temp.left
+            if temp is None:
+                return False
 
-    def print_value(self):
-        pass
 
-    def rebuild_tree(self):
-        pass
+tree = BinaryTree()
+tree.push(5)
+tree.push(7)
+tree.push(3)
+tree.push(1)
+tree.push(10)
+print(tree.size())
+tree.print_values()
+print()
+print(tree.contains(3))
